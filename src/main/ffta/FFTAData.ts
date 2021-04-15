52dce48fca12db4d6af2ac31865f96b8c2556370
fgkeiji <fgkeiji@gmail.com>
2021-04-14 22:11:38 -0700

    Added String Names to read item name from

--- src/main/ffta/FFTAData.ts
+++ src/main/ffta/FFTAData.ts
@@ -5,16 +5,17 @@ import * as FFTAUtils from "./FFTAUtils";
 // References
 const enum BYTELENGTH {
   ITEM = 0x20,
-  ITEMNAMES = 0x1c1d,
+  STRINGTABLE = 0x32,
 }
 
 const enum KNOWNOFFSET {
   ITEM = 0x51d1a0,
-  ITEMNAMES = 0x52396a,
+  STRINGTABLE = 0x526680,
 }
 
 const enum QUANTITY {
   ITEM = 375,
+  STRINGTABLE = 0,
 }
 
 // Common Properties
@@ -28,18 +29,30 @@ export interface FFTAObject {
 export class FFTAData {
   rom: Uint8Array;
   items: Array<FFTAItem>;
-  itemNames: Array<string>;
+  stringNames: Array<string>;
+  stringTable: Uint32Array;
 
   constructor(buffer: Uint8Array) {
     this.rom = buffer;
+    this.stringNames = [];
 
-    // Read in Item Names
-    this.itemNames = FFTAUtils.decodeFFTAText(
-      buffer.slice(
-        KNOWNOFFSET.ITEMNAMES,
-        KNOWNOFFSET.ITEMNAMES + BYTELENGTH.ITEMNAMES
-      )
-    );
+    let stringLookupTable = buffer.slice(KNOWNOFFSET.STRINGTABLE, 0x527244);
+    for (var i = 0; i < stringLookupTable.length; i+=4) {
+      let address = new Uint32Array([
+        (stringLookupTable[i + 3] << 24) |
+          (stringLookupTable[i + 2] << 16) |
+          (stringLookupTable[i + 1] << 8) |
+          stringLookupTable[i],
+      ]);
+
+      let startingByte = address[0] - 0x08000000;
+      let endingByte = startingByte;
+      do {
+        endingByte += 0x01;
+      }while (buffer[endingByte] !== 0);
+
+      this.stringNames.push(FFTAUtils.decodeFFTAText(buffer.slice(startingByte, endingByte)));
+    }
 
     // Initialize Items
     this.items = new Array<FFTAItem>();
@@ -48,7 +61,7 @@ export class FFTAData {
       let newItem = new FFTAItem(
         memory,
         i + 1,
-        this.itemNames[i],
+        this.stringNames[(buffer[memory+1] << 8) | buffer[memory]],
         buffer.slice(memory, memory + BYTELENGTH.ITEM)
       );
       this.items.push(newItem);
