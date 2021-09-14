7cc6bcd748bd942fa0a7f071364708c84131ecb7
fgkeiji <fgkeiji@gmail.com>
2021-09-13 17:04:36 -0700

    Added required mission item functionality

--- src/renderer/components/MissionLog.tsx
+++ src/renderer/components/MissionLog.tsx
@@ -5,10 +5,101 @@ import MissionView from "./MissionView";
 // @ts-ignore
 const { api } = window;
 
+const requiredMissionItems = [
+  "Adaman Alloy",
+  "Adamantite",
+  "Ahriman Eye",
+  "Ahriman Wing",
+  "Ally Finder",
+  "Ally Finder2",
+  "Ancient Bills",
+  "Ancient Medal",
+  "Animal Bone",
+  "Bent Sword",
+  "Black Thread",
+  "Blod Apple",
+  "Blood Apple",
+  "Blood Shawl",
+  "Body Ceffyl",
+  "Bomb Shell",
+  "Broken Sword",
+  "Caravan Musk",
+  "Cat's Tears",
+  "Choco Bread",
+  "Choco Gratin",
+  "Chocobo Egg",
+  "Chocobo Skin",
+  "Clock Gear",
+  "Clock Post",
+  "Crusite Alloy",
+  "Dame's Blush",
+  "Danbukwood",
+  "Delta Fang",
+  "Dictionary",
+  "Dragon Bone",
+  "Earth Sigil",
+  "Elda's Cup",
+  "Fairy Wing",
+  "Feather Badge",
+  "Fire Sigil",
+  "Flower Vase",
+  "Goldcap",
+  "Grownup Bread",
+  "Gun Gear",
+  "Gyshal Greens",
+  "Helje Key",
+  "Homework",
+  "Insignia",
+  "Jerky",
+  "Kiddy Bread",
+  "Leestone",
+  "Life Water",
+  "Magic Cloth",
+  "Magic Cotton",
+  "Magic Medal",
+  "Malboro Wine",
+  "Materite",
+  "Mind Ceffyl",
+  "Monster Guide",
+  "Moon Bloom",
+  "Moonwood",
+  "Mysidia Alloy",
+  "Mythril Pick",
+  "Neighbor Pin",
+  "Old Statue",
+  "Rabbit Tail",
+  "Rainbowite",
+  "Rat Tail",
+  "Runba's Tale",
+  "Rusty Spear",
+  "Rusty Sword",
+  "Secret Books",
+  "Silk Bloom",
+  "Silvril",
+  "Skull",
+  "Spirit Stone",
+  "Spiritstone",
+  "Stasis Rope",
+  "Stilpool Scroll",
+  "Stolen Gil",
+  "Stormstone",
+  "Stradivari",
+  "Sylvril",
+  "Thunderstone",
+  "Tonberry Lamp",
+  "Tranquil Box",
+  "Water Sigil",
+  "White Thread",
+  "Wind Sigil",
+  "Wyrmstone",
+  "Zodiac Ore",
+];
+
 export const MissionLog = () => {
   const [allMissions, setAllMissions] = React.useState(new Array<any>());
   const [missionItems, setMissionItems] = React.useState({} as any);
   const [showCompletedMissions, setShowCompleted] = React.useState(false);
+
   function updateAllMissions(updatedMission: any, amount: number) {
     setAllMissions(
       allMissions.map((mission) => {
@@ -29,10 +120,36 @@ export const MissionLog = () => {
         ? missionItems[rewardName] + amount
         : amount;
       newItems = { ...newItems, [rewardName]: newAmount };
+      if (newAmount <= 0) {
+        delete newItems[rewardName];
+      }
     });
+
     setMissionItems({
       ...newItems,
     });
+  }
+
+  function missionItemReqsMet(
+    missionItem1: string,
+    missionItem2: string
+  ): boolean {
+    if (missionItem1 == "" && missionItem2 == "") {
+      return true;
+    } else if (missionItem1 === missionItem2 && missionItem1 != "") {
+      return missionItems[missionItem1] - 2 >= 0;
+    } else if (missionItem1 != "" && missionItem2 != "") {
+      return (
+        missionItems[missionItem1] - 1 >= 0 &&
+        missionItems[missionItem2] - 1 >= 0
+      );
+    } else if (missionItem1 != "" && missionItem2 === "") {
+      return missionItems[missionItem1] - 1 >= 0;
+    } else if (missionItem1 === "" && missionItem2 != "") {
+      return missionItems[missionItem2] - 1 >= 0;
+    } else {
+      throw new Error("Required item not handled;");
+    }
   }
 
   React.useEffect(() => {
@@ -75,19 +192,25 @@ export const MissionLog = () => {
                 mission={mission}
                 updateAllMissions={updateAllMissions}
                 updateMissionItems={updateMissionItems}
+                missionItemReqsMet={missionItemReqsMet}
               />
             );
           }
         })}
       </div>
       <aside className="mission-item-view">
-        {Object.entries(missionItems).map((entry) => {
-          return (
-            <div>
-              {entry[0]}: {entry[1]}
-            </div>
-          );
-        })}
+        <div>
+          {Object.entries(missionItems)
+            .filter((element) => requiredMissionItems.includes(element[0]))
+            .sort()
+            .map((entry) => {
+              return (
+                <div>
+                  {entry[0]}: {entry[1]}
+                </div>
+              );
+            })}
+        </div>
       </aside>
     </div>
   ) : (
--- src/renderer/components/MissionView.tsx
+++ src/renderer/components/MissionView.tsx
@@ -5,7 +5,8 @@ import * as React from "react";
 const { api } = window;
 
 export const MissionLog = (props: any) => {
-  const { mission, updateAllMissions, updateMissionItems } = props;
+  const { mission, updateAllMissions, updateMissionItems, missionItemReqsMet } =
+    props;
 
   const columnKeys = [
     ["Pick Up Location", "Pick Up Month", "Mission Area"],
@@ -48,40 +49,49 @@ export const MissionLog = (props: any) => {
           `x${mission.Completed}`}
       </h3>
       {(mission.Completed == 0 ||
-        (mission.Completed > 0 && mission.Repeatable == "Yes")) && (
-        <div className="mission-complete">
-          <button
-            onClick={(event) => {
-              updateAllMissions(mission, 1);
-              let itemRewards = {} as any;
+        (mission.Completed > 0 && mission.Repeatable == "Yes")) &&
+        missionItemReqsMet(
+          mission["Required Item 1"],
+          mission["Required Item 2"]
+        ) && (
+          <div className="mission-complete">
+            <button
+              onClick={(event) => {
+                updateAllMissions(mission, 1);
+                let itemRewards = {} as any;
 
-              const updateItemRewards = (itemName: string, amount: number) => {
-                if (itemName != "")
-                  if (itemRewards[itemName]) {
-                    itemRewards = {
-                      ...itemRewards,
-                      [itemName]: itemRewards[itemName] + amount,
-                    };
-                  } else {
-                    itemRewards = {
-                      ...itemRewards,
-                      [itemName]: amount,
-                    };
-                  }
-              };
+                const updateItemRewards = (
+                  itemName: string,
+                  amount: number
+                ) => {
+                  if (itemName != "")
+                    if (itemRewards[itemName]) {
+                      itemRewards = {
+                        ...itemRewards,
+                        [itemName]: itemRewards[itemName] + amount,
+                      };
+                    } else {
+                      itemRewards = {
+                        ...itemRewards,
+                        [itemName]: amount,
+                      };
+                    }
+                };
 
-              updateItemRewards(mission["Item Reward 1"], 1);
-              updateItemRewards(mission["Item Reward 2"], 1);
+                updateItemRewards(mission["Required Item 1"], -1);
+                updateItemRewards(mission["Required Item 2"], -1);
+                updateItemRewards(mission["Item Reward 1"], 1);
+                updateItemRewards(mission["Item Reward 2"], 1);
 
-              if (Object.entries(itemRewards).length > 0) {
-                updateMissionItems(itemRewards);
-              }
-            }}
-          >
-            Complete
-          </button>
-        </div>
-      )}
+                if (Object.entries(itemRewards).length > 0) {
+                  updateMissionItems(itemRewards);
+                }
+              }}
+            >
+              Complete
+            </button>
+          </div>
+        )}
       {columnKeys.map((column) => (
         <div className="mission-readOnly">
           {column.map((name) =>
